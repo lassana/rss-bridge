dceaa736416300f5de9cd54a58017aa8cdfbb5cc
Mikalai Daronin <me@mikalaidaronin.info>
2021-11-30 13:27:42 +0300

    Add a new bridge for 'rrb.by' website.

---
+++ bridges/RrbBankByBridge.php
@@ -0,0 +1,28 @@
+<?php
+class RrbBankByBridge extends BridgeAbstract {
+
+	const MAINTAINER = 'lassana';
+	const NAME = 'Новости для клиентов Банка РРБ';
+	const URI = 'https://www.rrb.by';
+	const DESCRIPTION = 'Gets the service news from RRB.by website.';
+	const CACHE_TIMEOUT = 0;
+
+	public function collectData(){
+		$mainPageUrl = self::URI . '/presscentr/news';
+		$html = getSimpleHTMLDOM($mainPageUrl)
+			or returnServerError('Could not request rrb.by.');
+
+		foreach($html->find('div.content-list-item') as $element){
+			$item = array();
+			$item['uid'] = 'urn:sha1:' . hash('sha1', $element->find('div.content-list-item-title', 0)->first_child()->innertext);
+			$item['title'] = $element->find('div.content-list-item-title', 0)->first_child()->innertext;
+			$item['timestamp'] = DateTime::createFromFormat(
+				'Y / m / d',
+				$element->find('div.content-list-item-date', 0)->innertext
+				)->getTimestamp();
+			$item['uri'] = self::URI . $element->find('div.content-list-item-title', 0)->first_child()->href;
+
+			$this->items[] = $item;
+		}
+	}
+}
