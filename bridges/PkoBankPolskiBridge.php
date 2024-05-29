14f1e58e5d630f86a2f2751c97751dd2b85ce1cd
Mikalai Daronin <me@mikalaidaronin.info>
2024-05-29 08:52:24 +0300

    [PkoBankPolskiBridge] Introduce a bridge for pkobp.pl news

---
+++ bridges/PkoBankPolskiBridge.php
@@ -0,0 +1,97 @@
+<?php
+class PkoBankPolskiBridge extends BridgeAbstract {
+
+	const MAINTAINER = 'lassana';
+	const NAME = 'PKO Bank Polski';
+	const URI = 'https://www.pkobp.pl';
+	const DESCRIPTION = 'PKO Bank Polski: Aktualności';
+	const CACHE_TIMEOUT = 0; // 1 hour
+	const PARAMETERS = array(
+		'News' => array(
+			'category' => array(
+				'name' => 'Kategorie',
+				'type' => 'list',
+				'title' => 'Kategorie',
+				'values' => array(
+					'Wszystkie' => '0',
+					'Bankowość elektroniczna' => '4',
+					'Bezpieczeństwo' => '3',
+					'Kariera' => '7'
+				),
+				'defaultValue' => '0'
+			),
+			'customers' => array(
+				'name' => 'Klienci',
+				'type' => 'list',
+				'title' => 'Klienci',
+				'values' => array(
+					'Wszystkie' => '0',
+					'Bankowość prywatna' => '8',
+					'Dzieci' => '7',
+					'Firmy i przedsiębiorstwa' => '2',
+					'IKO' => '6',
+					'Indywidualni' => '1',
+					'iPKO' => '4',
+					'iPKO biznes' => '5',
+					'Korporacje i samorządy' => '3'
+				),
+				'defaultValue' => '0'
+			)
+		)
+	);
+
+	public function collectData() {
+		$category = $this->getInput('category');
+		$customers = $this->getInput('customers');
+
+		$mainPageUrl = self::URI . '/api/news/items?page_size=8&page_id=649';
+		if ($category != '0') {
+			$mainPageUrl .= '&categories=' . $category;
+		}
+		if ($customers != '0') {
+			$customers .= '&customers=' . $customers;
+		}
+		$mainPageUrl .= '&variant=contents';
+		$jsonContent = getContents($mainPageUrl);
+		$json = Json::decode($jsonContent);
+		$limit = 0;
+
+		foreach($json['results'] as $element) {
+			if($limit < 10) {
+				$item = array();
+				$itemUrl = self::URI . $element['path'];
+				$item['uid'] = 'urn:sha1:' . hash('sha1', $itemUrl);
+				$item['uri'] = $itemUrl;
+				$item['title'] = $element['snippet']['title']['text'];
+				$content = '<div>';
+				$label = $element['snippet']['label'];
+				$labelColor = $element['snippet']['label_color'];
+				error_log($label);
+				error_log($labelColor);
+				if (!empty($label) && $labelColor != '#F2F2F2') {
+					$content .= '<span style="color: #fff; background-color: '
+						. $labelColor
+						. '; border-radius: 4px; padding: 4px; margin-right: 4px;">'
+						. $label
+						. '</span>';
+				}
+				$content .= '<span> '
+					. $element['snippet']['lead']
+					. '</span>';
+				$content .= '</div>';
+				$item['content'] = $content;
+				$item['timestamp'] = DateTime::createFromFormat(
+					DateTime::ATOM,
+					$element['snippet']['publication_date']
+					)->getTimestamp();
+
+				$this->items[] = $item;
+				$limit++;
+			}
+		}
+	}
+
+	public function getIcon() {
+		return static::URI . '/static/redesign/_front/_img/_layout/favicon_new.png';
+	}
+}
